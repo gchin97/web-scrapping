7cb41f63786b2daa92ca703d3df92fba732d4640
gchin97 <graceschinn@gmail.com>
2023-04-03 14:57:30 +0900

    web

---
+++ 3_requests.py
@@ -0,0 +1,9 @@
+import requests
+res = requests.get("http://naver.com")
+print("응답코드: ", res.status_code)
+res.raise_for_status()
+# if res.status_code == 200:  # requests.codes.ok:
+#     print("정답입니다")
+# else:
+#     print("문제가 생겼습니다. [에러코드", res.status_code, "]")
+for ofr
